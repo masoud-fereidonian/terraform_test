8efe1ec580bcd9ff573d1af0a3b8e9baa89f1f20
masoud <mfereidonian@lululemon.com>
2018-06-11 16:26:50 -0700

    basic change

---
+++ resources.tf
@@ -0,0 +1,14 @@
+# resource "google_compute_network" "our_development_network" {
+#   name                    = "devnetwork"
+#   auto_create_subnetworks = true
+# }
+
+resource "aws_vpc" "environment_example_two" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    name = "Terrafrom-test"
+  }
+}
