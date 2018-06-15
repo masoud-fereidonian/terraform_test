7e490cabd29980f252ae5bd48c1350fc9c90e45c
masoud <mfereidonian@lululemon.com>
2018-06-15 15:05:43 -0700

    aws-update

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "our_development_network" {
   name                    = "devnetwork"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
 }
 
 resource "aws_vpc" "environment_example_two" {
@@ -10,5 +10,31 @@ resource "aws_vpc" "environment_example_two" {
 
   tags {
     name = "Terrafrom-test"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block        = "${cidrsubnet(aws_vpc.environment_example_two.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.environment_example_two.id}"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${cidrsubnet(aws_vpc.environment_example_two.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.environment_example_two.id}"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment_example_two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment_example_two.cidr_block}",
+    ]
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
   }
 }
