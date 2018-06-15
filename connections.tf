621505aa744a006d2099ea4ec071aa13e05e2ecb
masoud <mfereidonian@lululemon.com>
2018-06-15 15:18:12 -0700

    update azure ids

--- connections.tf
+++ connections.tf
@@ -9,8 +9,13 @@ provider "aws" {
 }
 
 provider "azurerm" {
-  subscription_id = "0"
-  client_id       = "1"
-  client_secret   = "1"
-  tenant_id       = "1"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
